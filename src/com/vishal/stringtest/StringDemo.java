883554796425e4615ee0e063967da762f7c9c9a4
Vishal Khatsuria <VKhatsuria@csc.com>
2018-05-21 10:59:43 +0530

    Initial commit

---
+++ src/com/vishal/stringtest/StringDemo.java
@@ -0,0 +1,13 @@
+package com.vishal.stringtest;
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+		if("abc" instanceof String){
+			System.out.println("It is instance of String");
+		}else{
+			System.out.println("Not a instance");
+		}
+	}
+
+}
